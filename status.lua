db95dc438f08d24af5d2842b75cc6e63ab2e5f69
LTUVR <116827369+TunelSeeker@users.noreply.github.com>
2024-08-20 17:16:30 +0300

    Create status.lua

---
+++ status.lua
@@ -0,0 +1,5 @@
+return {
+    -- Format: [gameID] = {status = "status", link = "scriptURL"}
+    ["17625359962"] = {status = "updated", link = "https://raw.githubusercontent.com/TunelSeeker/RIVALS/main/mainscript.lua"},
+    ["6969696969"] = {status = "updating", link = "fart"}
+}
